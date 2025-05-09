735e1fc74b03ebbe13905dd0ea2470d009c1b569
lowatell School <lowatell@student.s19.be>
2025-05-09 09:09:54 +0200

    Fix: update meal number after sleep(time_to_eat)

--- philo/routine.c
+++ philo/routine.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 09:13:56 by lowatell          #+#    #+#             */
-/*   Updated: 2025/05/08 18:34:41 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/05/09 09:08:23 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,13 @@ void	*p_eat(t_philo *philo)
 	pthread_mutex_lock(&philo->data->print);
 	printf("%ld %ld is eating\n", (gettime() - philo->data->s_time), philo->id);
 	pthread_mutex_unlock(&philo->data->print);
-	pthread_mutex_lock(&philo->data->count);
-	philo->meal_nb += 1;
-	pthread_mutex_unlock(&philo->data->count);
 	pthread_mutex_lock(&philo->data->last);
 	philo->lst_meal = gettime();
 	pthread_mutex_unlock(&philo->data->last);
 	optiusleep(philo->data->time_to_eat, gettime(), philo);
+	pthread_mutex_lock(&philo->data->count);
+	philo->meal_nb += 1;
+	pthread_mutex_unlock(&philo->data->count);
 	pthread_mutex_unlock(&philo->l_fork->fork);
 	pthread_mutex_unlock(&philo->r_fork->fork);
 	return (NULL);
